6cb795d1d0f5ce6745aab167cd931272f255f735
JAY-RATHOD-154 <jayrathod154@gmail.com>
2025-01-16 11:10:28 +0530

    DatePicker and TimePicker dialog added and other ways of writting onClick Listeners are also added for multiple Buttons

--- app/src/main/java/com/example/dialogsbutton/MainActivity.java
+++ app/src/main/java/com/example/dialogsbutton/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.dialogsbutton;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -41,34 +46,88 @@ public class MainActivity extends AppCompatActivity {
                 builder.setTitle("Exam Form Submission");
                 builder.setMessage("Are you sure you want to submit?");
                 builder.setIcon(R.drawable.ic_launcher_background);
-                builder.setPositiveButton("Yes",new PositiveButtonClickListener());
-                builder.setNegativeButton("No",new NegativeButtonClickListener());
-                builder.setNeutralButton("Ok", new NeutralButtonClickListener());
+                builder.setPositiveButton("Yes",new AlertDialogButtonClickListener());
+                builder.setNegativeButton("No",new AlertDialogButtonClickListener());
+     //Way 3 of passing object of anonymous class using dialogue interface
+                builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"Neutral Button Pressed!"+i,Toast.LENGTH_LONG).show();
+                    }
+                });
 
                 AlertDialog alertDialog = builder.create();
                 alertDialog.show();
+
+
+            }
+        });
+
+        btnTimePickerDialog.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog  timePickerDialog  = new TimePickerDialog(MainActivity.this,new TimePickerDialogButtonOnClickListener(),11,34,true);
+                timePickerDialog.show();
+
+            }
+        });
+
+        btnDatePickerDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,new DatePickerDialogueOnClickListener(),2025,01,16);
+                datePickerDialog.show();
             }
         });
     }
-
-    class PositiveButtonClickListener implements DialogInterface.OnClickListener{
+   //Way 2 of Writing code redundently for multiple Buttons
+    class AlertDialogButtonClickListener implements DialogInterface.OnClickListener{
         @Override
         public void onClick(DialogInterface dialogInterface, int which) {
-            Toast.makeText(MainActivity.this, "Positive Btn Clicked  " + which, Toast.LENGTH_LONG).show();
+            if(which == -1){
+                Toast.makeText(MainActivity.this,"Positive Button Clicked!",Toast.LENGTH_LONG).show();
+            } else if (which == -2) {
+                Toast.makeText(MainActivity.this,"Negative Button Clicked!",Toast.LENGTH_LONG).show();
+
+            }else {
+                Toast.makeText(MainActivity.this,"Neutral Button CLicked",Toast.LENGTH_LONG).show();
+            }
         }
     }
 
-    class NegativeButtonClickListener implements DialogInterface.OnClickListener{
-        @Override
-        public void onClick(DialogInterface dialogInterface, int which) {
-            Toast.makeText(MainActivity.this,"Negative Btn Clicked  " + which, Toast.LENGTH_LONG).show();
-        }
-    }
+//      Way 1- of Writing code for different Buttons
+//    class PositiveButtonClickListener implements DialogInterface.OnClickListener{
+//        @Override
+//        public void onClick(DialogInterface dialogInterface, int which) {
+//            Toast.makeText(MainActivity.this, "Positive Btn Clicked  " + which, Toast.LENGTH_LONG).show();
+//        }
+//    }
+//
+//    class NegativeButtonClickListener implements DialogInterface.OnClickListener{
+//        @Override
+//        public void onClick(DialogInterface dialogInterface, int which) {
+//            Toast.makeText(MainActivity.this,"Negative Btn Clicked  " + which, Toast.LENGTH_LONG).show();
+//        }
+//    }
+//
+//    class NeutralButtonClickListener implements DialogInterface.OnClickListener{
+//        @Override
+//        public void onClick(DialogInterface dialogInterface, int which) {
+//            Toast.makeText(MainActivity.this,"Neutral Btn Clicked  " + which,Toast.LENGTH_LONG).show();
+//        }
+//    }
+    class TimePickerDialogButtonOnClickListener implements TimePickerDialog.OnTimeSetListener {
 
-    class NeutralButtonClickListener implements DialogInterface.OnClickListener{
+    @Override
+    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+        Log.e("tag","timePicker"+" "+timePicker+" "+i+" "+i1);
+    }
+}
+    class DatePickerDialogueOnClickListener implements DatePickerDialog.OnDateSetListener{
         @Override
-        public void onClick(DialogInterface dialogInterface, int which) {
-            Toast.makeText(MainActivity.this,"Neutral Btn Clicked  " + which,Toast.LENGTH_LONG).show();
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            Log.e("Tag","datePicker"+" "+datePicker+" "+i+" "+i1+" "+i2);
         }
     }
 }
